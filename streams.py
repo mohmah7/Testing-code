165207e73f802b503f072d1490f3f5ca2c8df1ce
mohmah7 <mohmah7@users.noreply.github.com>
2018-11-15 15:57:06 +0200

    Create  streams.py

---
+++ streams.py
@@ -0,0 +1,17 @@
+import sys
+
+while True:
+  # output to stdout:
+  print "Yet another iteration ..."
+  try:
+    # reading from sys.stdin (stop with Ctrl-D):
+    number = raw_input("Enter a number: ")
+  except EOFError:
+    print "\nciao"
+    break
+  else:
+    number = int(number)
+    if number == 0:
+      print >> sys.stderr, "0 has no inverse"
+    else:
+      print "inverse of %d is %f" % (number, 1.0/number) 
