b6999763ff315f61403a16848d925574fa41c11d
mohmah7 <mohmah7@users.noreply.github.com>
2018-11-16 23:43:15 +0200

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -21,7 +21,7 @@ while True:
 
 print("Message length is", len(msg))
 
-server = smtplib.SMTP('localhost')
+server = smtplib.SMTP('outlook.com')
 server.set_debuglevel(1)
 server.sendmail(fromaddr, toaddrs, msg)
 server.quit()
