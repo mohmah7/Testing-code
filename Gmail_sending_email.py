a7625a384e3d1774925c038df819fba896328fe0
mohmah7 <mohmah7@users.noreply.github.com>
2018-11-19 22:43:28 +0200

    Update Gmail_sending_email.py

--- Gmail_sending_email.py
+++ Gmail_sending_email.py
@@ -6,7 +6,7 @@ gmail_password = input('PLEASE ENETET YOUR PASSWORD:')
 #try:  
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.ehlo()
-server.starttls()
+server.set_debuglevel(1)
 server.ehlo()
 server.login(gmail_user, gmail_password)
 print(' welcome email , the  password is :%s'% gmail_password)
