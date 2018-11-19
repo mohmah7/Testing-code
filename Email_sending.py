5c5e8e21e1f02c0e4c9b6f72e5760f5b2b5e95dc
mohmah7 <mohmah7@users.noreply.github.com>
2018-11-19 17:00:49 +0200

    Create Email_sending.py

---
+++ Email_sending.py
@@ -0,0 +1,9 @@
+
+import smtplib
+try:  
+    server = smtplib.SMTP('smtp.yahoo.com', 587)
+    server.ehlo()
+    server.starttls()
+    # ...send emails
+except:  
+    print 'Something went wrong...'
