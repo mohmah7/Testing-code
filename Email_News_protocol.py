504fbac683d8f8486cf30dead0753e588c31c0e3
mohmah7 <mohmah7@users.noreply.github.com>
2018-11-19 12:59:46 +0200

    Update Email_News_protocol.py

--- Email_News_protocol.py
+++ Email_News_protocol.py
@@ -1,3 +1,4 @@
 from nntplib import NNTP
-with NNTP('news.gmane.org') as n:
-     n.group('gmane.comp.python.committers')
+#with NNTP('news.gmane.org') as n:
+#     n.group('gmane.comp.python.committers')
+N = NNTP('mail.yahoo.com', user= 'mohmah7', pass= 'test')
