e2abba0db66d701a04e35e89b3009546d60e25f6
mohmah7 <mohmah7@users.noreply.github.com>
2018-11-19 09:14:02 +0200

    Update socket_client.py

--- socket_client.py
+++ socket_client.py
@@ -1,7 +1,7 @@
 # Echo client program
 import socket
 
-HOST = 'daring.cwi.nl'    # The remote host
+HOST = 'mail.yahoo.com'    # The remote host
 PORT = 50007              # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
