2a82cb36a146682811a6daead2227f6358403011
mohmah7 <mohmah7@users.noreply.github.com>
2018-11-19 18:06:55 +0200

    Update Gmail_sending_email.py

--- Gmail_sending_email.py
+++ Gmail_sending_email.py
@@ -3,10 +3,10 @@ import smtplib
 gmail_user = 'fiversameh@gmail.com'  
 gmail_password = input('PLEASE ENETET YOUR PASSWORD:')
 
-try:  
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(gmail_user, gmail_password)
-    print(' welcome email , the  password is :%s'% gmail_password)
-except:  
-    print('Something went wrong...The password is%s' %  gmail_password )
+#try:  
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.ehlo()
+server.login(gmail_user, gmail_password)
+print(' welcome email , the  password is :%s'% gmail_password)
+#except:  
+print('Something went wrong...The password is%s' %  gmail_password )
